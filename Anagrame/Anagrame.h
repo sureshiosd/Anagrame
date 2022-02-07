55d140dc5501a578e81cb7df2ee707f8a0bb442e
Suresh Patel <suresh.patel@vesta.io>
2022-02-07 12:22:12 +0530

    Initial Commit

---
+++ Anagrame/Anagrame.h
@@ -0,0 +1,18 @@
+//
+//  Anagrame.h
+//  Anagrame
+//
+//  Created by Suresh Patel on 07/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Anagrame.
+FOUNDATION_EXPORT double AnagrameVersionNumber;
+
+//! Project version string for Anagrame.
+FOUNDATION_EXPORT const unsigned char AnagrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Anagrame/PublicHeader.h>
+
+
